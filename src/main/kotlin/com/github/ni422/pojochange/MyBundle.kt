f391edd7566baf8658477a2b62484ce366b66b3b
GitHub Action <action@github.com>
2022-08-05 08:17:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/ni422/pojochange/MyBundle.kt
+++ src/main/kotlin/com/github/ni422/pojochange/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ni422.pojochange
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ni422/pojochange/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ni422/pojochange/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ni422.pojochange.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ni422.pojochange.MyBundle
 
 class MyApplicationService {
 
